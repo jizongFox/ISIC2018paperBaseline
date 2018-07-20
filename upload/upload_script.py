1021a624cdc7aacdd0a3ca4fbab3335076f14694
jizong <pengjizong@sjtu.edu.cn>
2018-07-20 15:34:58 -0400

    update the upload script using selenium and chrome.

--- upload/upload_script.py
+++ upload/upload_script.py
@@ -1,7 +1,8 @@
-#coding=utf8
+# coding=utf8
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time,requests,pickle,os
+import time, requests, pickle, os, pandas as pd
+from io import StringIO
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
@@ -13,19 +14,20 @@ HEADERS = {
 
 
 class automatic_upload(object):
-    def __init__(self) -> None:
+    def __init__(self,filename) -> None:
         super().__init__()
-        self.page_to_start = 'https://challenge.kitware.com/#phase/5b1c193356357d41064da2ec'
+        # self.page_to_start = 'https://challenge.kitware.com/#phase/5b1c193356357d41064da2ec'  # final test
+        self.page_to_start = 'https://challenge.kitware.com/#phase/5b1c16ca56357d41064da2e7'
         self.id = 'jizong'
         self.pw = '911005'
         self.session = requests.Session()
         self.header = HEADERS
         self.session.headers = self.header
-        self.implicitly_wait_time =5
-        self.cookies_dict={}
+        self.implicitly_wait_time = 5
+        self.cookies_dict = {}
+        self.filename= filename
 
-
-    def slow_input(self,ele,str):
+    def slow_input(self, ele, str):
         '''减慢账号密码的输入速度'''
         ele.send_keys(Keys.DELETE)
         for i in str:
@@ -34,14 +36,8 @@ class automatic_upload(object):
 
     def get_cookies(self):
         cookies = self.driver.get_cookies()
-        # cookies_dict = {}
-        # for cookie in cookies:
-        #     if 'name' in cookie and 'value' in cookie:
-        #         cookies_dict[cookie['name']] = cookie['value']
-        #
-        # self.cookies_dict = cookies_dict
-        # pickle.dump(self.driver.get_cookies(), open("cookies.pkl", "wb"))
-        self.cookies_dict=cookies
+        self.cookies_dict = cookies
+
     def set_cookies(self):
         '''将获取到的cookies加入session'''
         # cookies = pickle.load(open("cookies.pkl", "rb"))
@@ -50,7 +46,10 @@ class automatic_upload(object):
             self.driver.add_cookie(cookie)
 
     def fill_login(self):
-        self.driver = webdriver.Chrome()
+        options = webdriver.ChromeOptions()
+        options.add_argument('headless')
+
+        self.driver = webdriver.Chrome(chrome_options=options)
         self.driver.implicitly_wait(self.implicitly_wait_time)
         self.driver.get(self.page_to_start)
         time.sleep(1)
@@ -59,11 +58,12 @@ class automatic_upload(object):
         time.sleep(1)
         id = self.driver.find_element_by_id('g-login')
         print('input login name')
-        self.slow_input(id,self.id)
+        time.sleep(1)
+        self.slow_input(id, self.id)
+        time.sleep(1)
         print('input password')
         pw = self.driver.find_element_by_id('g-password')
-        pw.clear()
-        self.slow_input(pw,self.pw)
+        self.slow_input(pw, self.pw)
         print('login')
         login = self.driver.find_element_by_id('g-login-button')
         login.click()
@@ -72,51 +72,72 @@ class automatic_upload(object):
         button = self.driver.find_element_by_id('c-submit-phase-dataset')
         button.click()
 
+        self.upload(fill=False)
 
-        self.upload()
+    def upload(self, fill):
+        time.sleep(2)
+        if fill:
+            print('descriptor')
+            try:
+                description = self.driver.find_element_by_xpath(
+                    '//*[@id="g-app-body-container"]/div[2]/div[2]/div[1]/div/select')
+            except Exception as e:
+                description = self.driver.find_element_by_xpath(
+                    '//*[@id="g-app-body-container"]/div[2]/div[2]/div[1]/div/input')
 
-    def upload(self):
-        time.sleep(5)
-        print('descriptor')
-        try:
-            description = self.driver.find_element_by_xpath('//*[@id="g-app-body-container"]/div[2]/div[2]/div[1]/div/select')
-        except Exception as e:
-            description = self.driver.find_element_by_xpath('//*[@id="g-app-body-container"]/div[2]/div[2]/div[1]/div/input')
-          #  '//*[@id="g-app-body-container"]/div[2]/div[2]/div[1]/div/select'
-
-        self.slow_input(description,'111')
-        # description.send_keys('1111')
-        teamname = self.driver.find_element_by_class_name('c-submission-organization-input')
-        self.slow_input(teamname,'dahuli')
-        # teamname.send_keys('dahuli')
-        url = self.driver.find_element_by_class_name('c-submission-organization-url-input')
-        self.slow_input(url,'dahuli.com')
-        # url.send_keys('www.dahuli.com')
-        arxiv = self.driver.find_element_by_class_name('c-submission-documentation-url-input')
-        # arxiv.send_keys('www.arxiv.dahuli.com')
-        self.slow_input(arxiv,'www.arxiv.dahuli.com')
-        externaldata = self.driver.find_element_by_xpath('//*[@id="g-app-body-container"]/div[2]/div[2]/label[2]/input')
-        time.sleep(0.5)
-        externaldata.click()
+            self.slow_input(description, '111')
+            # description.send_keys('1111')
+            teamname = self.driver.find_element_by_class_name('c-submission-organization-input')
+            self.slow_input(teamname, 'dahuli')
+            # teamname.send_keys('dahuli')
+            url = self.driver.find_element_by_class_name('c-submission-organization-url-input')
+            self.slow_input(url, 'dahuli.com')
+            # url.send_keys('www.dahuli.com')
+            try:
+                arxiv = self.driver.find_element_by_class_name('c-submission-documentation-url-input')
+                # arxiv.send_keys('www.arxiv.dahuli.com')
+                self.slow_input(arxiv, 'www.arxiv.dahuli.com')
+            except:
+                pass
+            externaldata = self.driver.find_element_by_xpath(
+                '//*[@id="g-app-body-container"]/div[2]/div[2]/label[2]/input')
+            time.sleep(0.5)
+            externaldata.click()
 
         ## agreement
-        agree = self.driver.find_element_by_xpath('//*[@id="g-app-body-container"]/div[2]/div[2]/div[5]/label/input')
+        try:
+            agree = self.driver.find_element_by_xpath(
+                '//*[@id="g-app-body-container"]/div[2]/div[2]/div[5]/label/input')
+        except:
+            agree = self.driver.find_element_by_xpath(
+                '//*[@id="g-app-body-container"]/div[2]/div[2]/div[4]/label/input')
         time.sleep(0.2)
         agree.click()
 
         file = self.driver.find_element_by_id('g-files')
-        file.send_keys(os.path.join(os.getcwd(),'ISIC_Segmenation_dataset_split.zip'))
+        file.send_keys(os.path.join(os.getcwd(), 'val.zip'))
         time.sleep(1)
         submit = self.driver.find_element_by_xpath('//*[@id="g-upload-form"]/div[8]/button')
         submit.click()
-        time.sleep(100)
+        # time.sleep(100)
+
+    def get_excel(self):
+        while True:
+            if self.driver.page_source.find('Overall score:') > 0:
+                print('We have the overall score')
+                break
+            else:
+                time.sleep(2)
+
+        excel = self.driver.find_element_by_xpath('//*[@id="g-app-body-container"]/div[2]/div[2]/table/tbody').text
+        excel = pd.read_csv(StringIO(excel), )
+        excel['image_id']=excel.iloc[:,0].apply(lambda x:x.split(' ')[0])
+        excel['image_score'] = excel.iloc[:,0].apply(lambda x:x.split(' ')[1])
+        excel.to_csv(self.filename.replace('zip','csv'),encoding='utf8')
+        self.driver.close()
 
 
-
-
-
-
-
-if __name__=="__main__":
-    a = automatic_upload()
+if __name__ == "__main__":
+    a = automatic_upload('val.zip')
     a.fill_login()
+    a.get_excel()
